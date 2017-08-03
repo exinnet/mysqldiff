13c2a8d08cd15d13c90ddcb4881afdf334c83dd4
king <chineselittleboy@gmail.com>
2017-08-03 23:38:53 +0800

    将配置文件从json转为php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+return [
+	"master" => [
+		"host" => "127.0.0.1",
+		"user" => "root",
+		"pwd" => "root",
+		"db" => "test",
+	],
+	"slave" => [
+		"host" => "127.0.0.1",
+		"user" => "root",
+		"pwd" => "root",
+		"db" => "test2",
+	],	
+];
